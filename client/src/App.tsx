abd8cad5aa1ea09c305a3b2a9748e8c49384bee0
hemat <hematullahw@gmail.com>
2024-09-22 21:08:00 +0430

    client folder for react

---
+++ client/src/App.tsx
@@ -0,0 +1,30 @@
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { Login } from "./pages/Login";
+import { Register } from "./pages/Register";
+import ProtectedPages from "./pages/ProtectedPages";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import NotFound from "./pages/NotFound";
+
+function App() {
+
+  document.title = "100 Khana";
+
+  return (
+    <>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<ProtectedPages />}>
+          <Route index element={<Home />} />
+          <Route path="profile/me" element={<Profile />} />
+          <Route path="profile/:id" element={<Profile />} />
+        </Route>
+        <Route path="*" element={<NotFound/>} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
