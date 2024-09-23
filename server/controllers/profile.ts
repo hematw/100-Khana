99e858b13dea69981bece891105b35a27fa0efb9
hemat <hematullahw@gmail.com>
2024-09-24 00:43:56 +0430

    profile controller created

---
+++ server/controllers/profile.ts
@@ -0,0 +1,55 @@
+import { Response } from "express";
+import asyncHandler from "../middlewares/async-handler";
+import Profile from "../models/Profile";
+import { AuthenticatedRequest } from "../types";
+import { JwtPayload } from "jsonwebtoken";
+import User from "../models/User";
+
+export const getProfile = asyncHandler(
+  async (req: AuthenticatedRequest, res: Response) => {
+    let userId = req.params.id;
+    if (userId === "me" && typeof req.user === "object") {
+      userId = req.user?.id;
+    }
+    const profile = await Profile.findOne({ userId }).populate("userId");
+    res.json({ success: true, profile });
+  }
+);
+
+export const updateProfile = asyncHandler(
+  async (req: AuthenticatedRequest, res: Response) => {
+    if (typeof req.user === "object") {
+      let userId: JwtPayload = req.user?.id;
+      console.log(userId);
+      console.log(req.body);
+      if (userId) {
+        try {
+          await User.findByIdAndUpdate(userId, {
+            email: req.body.email,
+          });
+          const profile = await Profile.findOneAndUpdate(
+            { userId },
+            { userId, ...req.body },
+            {
+              new: true,
+              runValidators: true,
+              upsert: true,
+            }
+          ).populate('userId');
+          if (!profile) {
+            res
+              .status(404)
+              .json({ success: false, message: "Profile not found" });
+          } else {
+            res.json({ success: true, profile: profile });
+          }
+        } catch (error) {
+          console.error(error);
+          res
+            .status(500)
+            .json({ success: false, message: "Failed to update profile" });
+        }
+      }
+    }
+  }
+);
