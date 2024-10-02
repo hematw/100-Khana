1561e84b94515f45096688da770f23d380c4c175
hemat <hematullahw@gmail.com>
2024-10-02 21:53:03 +0430

    home router created

--- server/types/AuthenticatedRequest.ts
+++ server/types/AuthenticatedRequest.ts
@@ -2,5 +2,5 @@ import { JwtPayload } from "jsonwebtoken";
 import { Request } from "express";
 
 export interface AuthenticatedRequest extends Request {
-  user?: JwtPayload | string;
+  user?: JwtPayload;
 }
